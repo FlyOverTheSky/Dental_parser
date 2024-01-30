2ef9171cdb0cb2c91fcc178b3047af2585673701
FlyOverTheSky <sashka.sheshenin@gmail.com>
2024-01-30 11:46:01 +0500

    Вытаскиваются цены и наименования товаров для пробахилы.рф

--- main.py
+++ main.py
@@ -6,10 +6,15 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
+OBJECTS_COUNT = 5
+
+
 browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 ChromeOptions = webdriver.ChromeOptions()
 # Отключаем все расширения Хрома
 ChromeOptions.add_argument("--disable-extensions")
+# Отключаем звук в браузере
+ChromeOptions.add_argument("--mute-audio")
 
 # Обход блокировки парсинга
 browser.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
@@ -31,4 +36,24 @@ find_button = browser.find_element(
     "/html/body/header/div[1]/div[2]/div/form/button"
 )
 WebDriverWait(browser, 10).until(EC.element_to_be_clickable(find_button)).click()
-time.sleep(30)
+
+find_results = browser.find_element(
+    By.XPATH,
+    "/html/body/main/div/div/div/div[3]/div[2]",
+)
+WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "products__item")))
+items = find_results.find_elements(
+    By.TAG_NAME,
+    "div",
+)
+for counter in range(OBJECTS_COUNT):
+    current_item = items[counter]
+    item_name = current_item.find_element(
+        By.XPATH,
+        f"/html/body/main/div/div/div/div[3]/div[2]/div[{counter + 1}]/a/span[2]/span[1]"
+    )
+    item_price = current_item.find_element(
+        By.XPATH,
+        f"/html/body/main/div/div/div/div[3]/div[2]/div[{counter + 1}]/div[2]/form/div[3]/div[1]"
+    )
+    print(item_name.text, item_price.text)
