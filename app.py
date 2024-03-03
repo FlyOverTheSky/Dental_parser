ff4132e6c0cf79c84ec5dae70f17481585194169
FlyOverTheSky <sashka.sheshenin@gmail.com>
2024-03-03 17:02:52 +0500

    Add comments and TODO

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ from parsers.probahily import ProbahilyParser
 from parsers.averon import AveronParser
 from settings import *
 
-
 app = FastAPI()
 parsers_list = []
 
@@ -28,6 +27,8 @@ async def get_cached_items(
         item_name: str,
         items_count: int,
         company_name: str):
+    """Метод для проверки есть ли эти данные в кэше."""
+    # TODO: Проверка в кэше по компании и количеству закэшированных резульатов.
     cached_results = await redis_connection.get(item_name)
     if not cached_results:
         return None
@@ -36,6 +37,7 @@ async def get_cached_items(
 
 @app.get("/parse/")
 async def parse_item(parse_request: ParseRequest):
+    """Парсинг по наименованию."""
     search_results = {}
 
     redis_connection = redis.Redis(
@@ -45,7 +47,7 @@ async def parse_item(parse_request: ParseRequest):
     cache_key = parse_request.item_name
 
     for parser in parsers_list:
-        # Переводить имя на англ для удобства проверки redis
+        # TODO: Переводить имя на англ для удобства проверки redis
         cached_results = await get_cached_items(
             redis_connection=redis_connection,
             item_name=cache_key,
@@ -53,20 +55,22 @@ async def parse_item(parse_request: ParseRequest):
             company_name=parser.company_name
         )
 
+        # Если в кэше есть нужные наименования
         if cached_results:
             search_results[parser.company_name] = parser.last_results
             continue
-
+        # Если в кэше нет данных, то запускаем парсер.
         parser.parse_names_and_prices(
             to_search=parse_request.item_name,
             return_items_count=parse_request.count_to_search
         )
         search_results[parser.company_name] = parser.last_results
 
+    # Кэшируем результаты поиска
     await redis_connection.setex(
         name=cache_key,
         value=json.dumps(search_results),
-        time=30,
+        time=CACHE_TIME_LIMIT,
     )
 
     return search_results
--- settings.py
+++ settings.py
@@ -10,3 +10,5 @@ REDIS_PORT = os.getenv("REDIS_PORT", 6379)
 PARSER_DIR = "parsers"
 AVERON_SITE_URL = "https://averon-td.ru/"
 PROBAHILY_SITE_URL = "https://xn--80abwmlfh7b4c.xn--p1ai/"
+
+CACHE_TIME_LIMIT = os.getenv("CACHE_TIME_LIMIT", 60)
