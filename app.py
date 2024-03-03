e986abce777394273387c362cf76b95574069b7c
FlyOverTheSky <sashka.sheshenin@gmail.com>
2024-03-03 14:20:30 +0500

    Add Redis-cache

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import redis.asyncio as redis
+import json
+from googletrans import Translator
 
 from parsers.probahily import ProbahilyParser
 from parsers.averon import AveronParser
 
 app = FastAPI()
-parsers_lits = []
+parsers_list = []
 
 probahily_parser = ProbahilyParser()
-parsers_lits.append(probahily_parser)
+parsers_list.append(probahily_parser)
 
 averon_parser = AveronParser()
-parsers_lits.append(averon_parser)
+parsers_list.append(averon_parser)
 
 
 class ParseRequest(BaseModel):
@@ -19,18 +22,50 @@ class ParseRequest(BaseModel):
     count_to_search: int
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def get_cached_items(
+        redis_connection,
+        item_name: str,
+        items_count: int,
+        company_name: str):
+    cached_results = await redis_connection.get(item_name)
+    if not cached_results:
+        return None
+    return json.loads(cached_results)
 
 
 @app.get("/parse/")
 async def parse_item(parse_request: ParseRequest):
     search_results = {}
-    for parser in parsers_lits:
+
+    redis_connection = redis.Redis(
+        host='127.0.0.1',
+        port=6379,
+    )
+    cache_key = parse_request.item_name
+
+    for parser in parsers_list:
+        # Переводить имя на англ для удобства проверки redis
+        cached_results = await get_cached_items(
+            redis_connection=redis_connection,
+            item_name=cache_key,
+            items_count=parse_request.count_to_search,
+            company_name=parser.company_name
+        )
+
+        if cached_results:
+            search_results[parser.company_name] = parser.last_results
+            continue
+
         parser.parse_names_and_prices(
             to_search=parse_request.item_name,
             return_items_count=parse_request.count_to_search
         )
         search_results[parser.company_name] = parser.last_results
+
+    await redis_connection.setex(
+        name=cache_key,
+        value=json.dumps(search_results),
+        time=30,
+    )
+
     return search_results
