8004c0250beefb0c4992756005459c81efacbfc5
Alexandr <115569310+FlyOverTheSky@users.noreply.github.com>
2024-03-21 06:33:04 +0500

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from backend.parsers.probahily import ProbahilyParser
-
-
-parser = ProbahilyParser()
-parser.parse_names_and_prices(
-    to_search="Перчатки",
-    return_items_count=10
-)
-print(parser.last_results)
